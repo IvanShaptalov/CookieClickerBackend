2ce9d9bcb21a1eb3eea0a5fb5ab3261d7cf8faf8
IvanShaptalov <ivan.shaptalov1@gmail.com>
2024-07-14 13:22:31 +0200

    feat(): added requirements.txt

--- app/routers/root.py
+++ app/routers/root.py
@@ -5,4 +5,4 @@ router = APIRouter()
 
 @router.get("/", tags=["root"])
 async def read_root():
-    return Response(status_code=status.HTTP_200_OK, content=json.dumps({"docs link": "http://127.0.0.1:8000/docs"}))
+    return Response(status_code=status.HTTP_200_OK, content=json.dumps({"docs link": "/docs"}))
